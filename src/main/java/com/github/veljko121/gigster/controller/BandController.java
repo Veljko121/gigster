4621b3d4eb18f9bda477e2cdc7f2c206f0aac2e3
Veljko Nikolić <nikolicveljko01@gmail.com>
2024-07-09 10:48:08 +0200

    Added delete mapping

--- src/main/java/com/github/veljko121/gigster/controller/BandController.java
+++ src/main/java/com/github/veljko121/gigster/controller/BandController.java
@@ -14,6 +14,7 @@ import java.util.Collection;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -41,6 +42,12 @@ public class BandController {
     public ResponseEntity<BandResponseDTO> create(@RequestBody BandRequestDTO requestDTO) {
         var responseDTO = bandService.save(requestDTO);
         return new ResponseEntity<>(responseDTO, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteById(@PathVariable Integer id) {
+        bandService.deleteById(id);
+        return ResponseEntity.ok().build();
     }
     
 }
